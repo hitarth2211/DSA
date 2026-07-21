feaee95be8a6b3de6b0e5dcd2572d1c47d27231e
Hitarth Shah <hitarthshah2211@gmail.com>
2026-07-21 20:42:50 +0530

    Time: 14 ms (68.91%), Space: 48.8 MB (13.24%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -3,9 +3,7 @@ class Solution {
         int n = nums.length;
         int[] res = new int[n];
         Arrays.fill(res, -1);
-        int j = 0;
         Stack<Integer> st = new Stack<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i <  2 * n; i++) {
             int idx = i % n;
             while(!st.isEmpty() && nums[idx] > nums[st.peek()]) {
