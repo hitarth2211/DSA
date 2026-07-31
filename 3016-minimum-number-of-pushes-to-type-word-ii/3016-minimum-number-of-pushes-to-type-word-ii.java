972c020e4559116fa21132d46c2ab9e290e2bd28
Hitarth Shah <hitarthshah2211@gmail.com>
2026-07-31 19:01:31 +0530

    Time: 8 ms (99.71%), Space: 48.1 MB (52.6%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,25 +1,43 @@
 class Solution {
     public int minimumPushes(String word) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a,b)->Integer.compare(b[1],a[1])
-        );
+        // PriorityQueue<int[]> pq = new PriorityQueue<>(
+        //     (a,b)->Integer.compare(b[1],a[1])
+        // );
+        // int[] freq = new int[26];
+        // for(int i = 0; i < word.length(); i++) {
+        //     char ch = word.charAt(i);
+        //     freq[ch - 'a']++;
+        // }
+        // for(int i = 0; i < 26; i++) {
+        //     pq.add(new int[]{i, freq[i]});
+        // }
+        // int res = 0, count = 1, idx = 2;
+        // while(!pq.isEmpty()) {
+        //     int[] arr = pq.poll();
+        //     char ch = (char)(arr[0] + 'a');
+        //     int fre = arr[1];
+        //     res += count * fre;
+        //     if(idx == 9){
+        //         idx = 2;
+        //         count++; 
+        //     }
+        //     else idx++;
+        // }
+        // return res;
+
+
         int[] freq = new int[26];
         for(int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
             freq[ch - 'a']++;
         }
-        for(int i = 0; i < 26; i++) {
-            pq.add(new int[]{i, freq[i]});
-        }
-        int res = 0, count = 1, idx = 2;
-        while(!pq.isEmpty()) {
-            int[] arr = pq.poll();
-            char ch = (char)(arr[0] + 'a');
-            int fre = arr[1];
-            res += count * fre;
-            if(idx == 9){
+        Arrays.sort(freq);
+        int res = 0, idx = 2, count = 1;
+        for(int i = 25; i >= 0; i--) {
+            res += freq[i] * count;
+            if(idx == 9) {
                 idx = 2;
-                count++; 
+                count++;
             }
             else idx++;
         }
