fe4316ca8b9bb7353cb589d7b6757a085bd11bcd
Hitarth Shah <hitarthshah2211@gmail.com>
2026-09-05 10:49:03 +0530

    Time: 3 ms (100%), Space: 133.3 MB (20.99%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,21 +1,17 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int res=Integer.MAX_VALUE;
         int n = nums.length;
-        int[] preMax = new int[n];
+        int max = 0;
         int[] sufMin = new int[n];
-        preMax[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            preMax[i] = Math.max(preMax[i - 1], nums[i]);
-        }
         sufMin[n - 1] = nums[n - 1];
         for(int i = n - 2; i >= 0; i--) {
             sufMin[i] = Math.min(nums[i], sufMin[i + 1]);
         }
         for(int i = 0; i < n; i++) {
-            if(preMax[i] - sufMin[i] <= k) 
-                res = Math.min(res, i);
+            max = Math.max(max, nums[i]);
+            if(max - sufMin[i] <= k) 
+                return i;
         }
-        return res == Integer.MAX_VALUE ? -1 : res;
+        return  -1;
     }
 }
