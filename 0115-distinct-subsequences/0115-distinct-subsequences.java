fb4dc2d5955632bb4f448f99f0b760ce0d89aeae
Hitarth Shah <hitarthshah2211@gmail.com>
2026-09-06 12:41:26 +0530

    Time: 19 ms (69.08%), Space: 54.4 MB (38.68%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,4 +1,5 @@
 class Solution {
+    // Backtracking
     // private void backtrack(int idx, String s, String t, StringBuilder sb) {
     //     if(sb.length() == t.length()) {
     //         String str = sb.toString();
@@ -14,24 +15,39 @@ class Solution {
     //     sb.deleteCharAt(sb.length() - 1);
     //     backtrack(idx + 1, s, t, sb);
     // }
-    int memo[][];
-    private int solve(int i, int j, String s, String t) {
-        if(j == t.length()) return 1;
-        if(i == s.length()) return 0;
-        if(memo[i][j] != -1) return memo[i][j];
-        int res = solve(i + 1, j, s, t);
-        if(s.charAt(i) == t.charAt(j)) {
-            res += solve(i + 1, j + 1, s, t);
-        }
-        return memo[i][j] = res;
-    }
+
+    // Memoization
+    // int memo[][];
+    // private int solve(int i, int j, String s, String t) {
+    //     if(j == t.length()) return 1;
+    //     if(i == s.length()) return 0;
+    //     if(memo[i][j] != -1) return memo[i][j];
+    //     int res = solve(i + 1, j, s, t);
+    //     if(s.charAt(i) == t.charAt(j)) {
+    //         res += solve(i + 1, j + 1, s, t);
+    //     }
+    //     return memo[i][j] = res;
+    // }
     public int numDistinct(String s, String t) {
         // StringBuilder sb = new StringBuilder();
         // backtrack(0, s, t, sb);
-        memo = new int[s.length()][t.length()];
-        for(int i = 0; i < s.length(); i++) {
-            Arrays.fill(memo[i], -1);
+        // memo = new int[s.length()][t.length()];
+        // for(int i = 0; i < s.length(); i++) {
+        //     Arrays.fill(memo[i], -1);
+        // }
+        int n = s.length(), m = t.length();
+        int[][] dp = new int[n + 1][m + 1]; 
+        for(int i = 0; i <= n; i++) dp[i][m] = 1;
+        for(int j = 0; j < m; j++) dp[n][j] = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j];
+                if(s.charAt(i) == t.charAt(j)) {
+                    dp[i][j] += dp[i + 1][j + 1]; 
+                }
+            }
         }
-        return solve(0, 0, s, t);
+
+        return dp[0][0];
     }
 }
